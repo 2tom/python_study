2d3e91e8905d2d538d78f68fd2dfd76d559ca29e
Shinyawy <k.shinya76@gmail.com>
2015-06-18 10:57:44 +0900

    add

---
+++ shinyawy/08_hello.py
@@ -0,0 +1,8 @@
+#coding: UTF-8
+#No.2 Python Study 2015/06/18
+sales = [255,100,353,400]
+
+print len(sales)
+print sales[2]
+print range(10)
+print range(3,10,2)
---
+++ shinyawy/09_hello.py
@@ -0,0 +1,20 @@
+#coding: UTF-8
+#No.2 Python Study 2015/06/18
+
+sales = [50,100,80,45]
+sales.reverse()
+print sales
+
+sales.sort()
+sales.reverse()
+print sales
+
+c="2015/6/18"
+print c.split("/")
+
+a=["a","b","c"]
+print "-".join(a)
+
+aaa=[50,"50",100,"100"]
+aaa.sort()
+print aaa
