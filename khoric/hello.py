873c5198426021e6f937e123a5bf037704a58fce
khoric <kenji.horita.k@ctc-g.co.jp>
2015-06-18 13:33:09 +0900

    seconf commit

---
+++ khoric/hello.py
@@ -0,0 +1,21 @@
+# comment
+# coding: UTF-8
+# コメント
+
+msg = "hello world"
+print msg
+
+print 10 * 5
+print 10 // 3
+print 10 % 3
+print 2 ** 3
+
+print 5 + 2.0
+print 10 // 3.0
+print 10 / 3.0
+
+print -10 // 3.0
+print -10 / 3.0
+
+print 10 // 3
+print -10 // 3
---
+++ khoric/list.py
@@ -0,0 +1,15 @@
+sales = [100, 200, 300, 400]
+
+print sales
+print len(sales)
+print sales[2]
+sales[3] = 1000
+print sales
+sales[0] = "horihori"
+sales[1] = range(3)
+print sales
+print 1000 in sales
+print 500 in sales
+
+print range(10)
+print range(3,10)
