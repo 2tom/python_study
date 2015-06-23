451c148ce1568a80afdcb0e270d6e794dbbf460c
Shinyawy <k.shinya76@gmail.com>
2015-06-23 21:39:07 +0900

    add

--- shinyawy/09_hello.py
+++ shinyawy/09_hello.py
@@ -1,18 +1,17 @@
 #coding: UTF-8
 #No.2 Python Study 2015/06/18
-
+# リスト
 sales = [50,100,80,45]
-sales.reverse()
-print sales
-
+# sort / reverse
 sales.sort()
+print sales
 sales.reverse()
 print sales
 
-c="2015/6/18"
-print c.split("/")
-
-a=["a","b","c"]
+# 文字列とリスト
+d="2015/6/18"
+print d.split("/")
+a=["a", "b", "c"]
 print "-".join(a)
 
 aaa=[50,"50",100,"100"]
